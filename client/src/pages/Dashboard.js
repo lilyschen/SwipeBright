ea2ed1aa98dceef2cb33046afb71c72f3f50e88f
saharahbains <saharahb@students.cs.ubc.ca>
2024-03-10 00:43:51 -0800

    dashboard changes

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,5 +1,21 @@
+import React, { useState } from 'react'
+import TinderCard from "react-tinder-card";
+import ChatContainer from "../components/ChatContainer";
+
 const Dashboard = () => {
-    <h2> DASHBOARD </h2>
+
+    const [lastDirection, setLastDirection] = useState()
+
+    const swiped = (direction, nameToDelete) => {
+        console.log('removing: ' + nameToDelete)
+        setLastDirection(direction)
+    }
+
+    const outOfFrame = (name) => {
+        console.log(name + ' left the screen!')
+    }
+
+
     const characters  = [
         {
             name: "Saharah Bains",
@@ -21,12 +37,28 @@ const Dashboard = () => {
 
     return (
         <div className="dashboard">
+            <ChatContainer/>
             <div className="swiper-container">
                 <div className="card-container">
+                    <h2> DASHBOARD </h2>
 
+                    {characters.map((character) =>
+                        <TinderCard
+                            className='swipe'
+                            key={character.name}
+                            onSwipe={(dir) => swiped(dir, character.name)}
+                            onCardLeftScreen={() => outOfFrame(character.name)}>
+                            <div style={{ backgroundImage: 'url(' + character.url + ')' }}
+                                 className='card'>
+                                <h3>{character.name}</h3>
+                            </div>
+                        </TinderCard>
+                    )}
+                    <div className="swipe-info">
+                        {lastDirection ? <p>You swiped {lastDirection}</p> : <p/>}
+                    </div>
                 </div>
             </div>
-
         </div>
     )
 }
