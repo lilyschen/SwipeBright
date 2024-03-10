8179590c3ee6ee874d6d135164ad208ab03d687a
saharahbains <saharahb@students.cs.ubc.ca>
2024-03-09 23:20:53 -0800

    dashboard changes

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,6 +1,33 @@
 const Dashboard = () => {
+    <h2> DASHBOARD </h2>
+    const characters  = [
+        {
+            name: "Saharah Bains",
+            url: './images.icon'
+        },
+        {
+            name: "Michelle Lei",
+            url: './images.icon'
+        },
+        {
+            name: "Allison F",
+            url: './images.icon'
+        },
+        {
+            name: "Lily Chen",
+            url: './images.icon'
+        }
+    ]
+
     return (
-        <div>Dashboard</div>
+        <div className="dashboard">
+            <div className="swiper-container">
+                <div className="card-container">
+
+                </div>
+            </div>
+
+        </div>
     )
 }
 export default Dashboard
